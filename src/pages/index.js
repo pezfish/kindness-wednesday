0c1f7ef925017b11aa00cfa0a9efa077c44489b4
Kevin Doyle <kevin.doyle@thinkcompany.com>
2020-05-11 13:30:50 -0400

    remove extra pages

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,63 +1,24 @@
 import React from 'react';
-import PropTypes from 'prop-types';
-import { Link, graphql } from 'gatsby';
+import { graphql } from 'gatsby';
 
 import Layout from '../components/Layout';
 
-export const IndexPageTemplate = ({
-    image,
-    title,
-    heading,
-    subheading,
-    mainpitch,
-    description,
-    intro,
-}) => (
+export const IndexPageTemplate = () => (
     <div>
-        <p>index page</p>
+        <h1>Kindness Wednesday</h1>
     </div>
 );
 
-IndexPageTemplate.propTypes = {
-    image: PropTypes.oneOfType([PropTypes.object, PropTypes.string]),
-    title: PropTypes.string,
-    heading: PropTypes.string,
-    subheading: PropTypes.string,
-    mainpitch: PropTypes.object,
-    description: PropTypes.string,
-    intro: PropTypes.shape({
-        blurbs: PropTypes.array,
-    }),
-};
-
 const IndexPage = ({ data }) => {
-    const { frontmatter } = data.markdownRemark;
+    console.log(data);
 
     return (
         <Layout>
-            <IndexPageTemplate
-                image={frontmatter.image}
-                title={frontmatter.title}
-                heading={frontmatter.heading}
-                subheading={frontmatter.subheading}
-                mainpitch={frontmatter.mainpitch}
-                description={frontmatter.description}
-                intro={frontmatter.intro}
-            />
+            <IndexPageTemplate />
         </Layout>
     );
 };
 
-IndexPage.propTypes = {
-    data: PropTypes.shape({
-        markdownRemark: PropTypes.shape({
-            frontmatter: PropTypes.object,
-        }),
-    }),
-};
-
-export default IndexPage;
-/*
 export const pageQuery = graphql`
     query IndexPageTemplate {
         markdownRemark(frontmatter: { templateKey: { eq: "index-page" } }) {
@@ -95,4 +56,5 @@ export const pageQuery = graphql`
         }
     }
 `;
-*/
+
+export default IndexPage;
